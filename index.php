5f1ab906fa4bb64365b6cfa2ba459df878a7e5c2
sujatahalder <sujata.halder@outlook.com>
2020-06-22 16:12:17 +0530

    new change

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
-echo "some files has been deleted from here";
+$db_host = "localhost";
+$db_id = "root";
+$db_pass = "";
+$db_name = "players";
+$conn = new mysqli($db_host,$db_id,$db_pass,$db_name);
+ 
+if($conn->connect_error){
+    die ("connection not build<br>");
+}else{
+    echo "database connected<br>";
+}
+
+$sql = "SELECT * FROM data_list";
+$result = $conn->query($sql);
+//echo $result-> num_rows;
+if($result-> num_rows>0){
+   while($row=$result->fetch_assoc()){
+    foreach($row as $key=>$val){
+        echo $key ." : " . $val . "<br>" ;
+    }
+    
+}
+}
+
 
 ?>
